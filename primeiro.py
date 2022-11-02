2a48dfdc375a5c359a46a941c1ced37716d7ab14
Higor <higor.b97@hotmail.com>
2022-11-02 20:56:23 -0300

    Feat: Finalização do segundo teste.

--- primeiro.py
+++ primeiro.py
@@ -9,18 +9,18 @@ arquivos = []
 
 def requisicao():
     try:
-        if os.path.isdir('./Anexos') == False:
+        if os.path.isdir("./Anexos") == False:
             os.mkdir("./Anexos")
 
-        conteudo = requests.get('https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude')
+        conteudo = requests.get("https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude")
         if conteudo.status_code != 200:
             return
 
-        site = BeautifulSoup(conteudo.text, 'html5lib')
+        site = BeautifulSoup(conteudo.text, "html5lib")
 
         for x in range(14, 19):
-            link = site.select(f'#parent-fieldname-text > p:nth-child({str(x)}) > a')[0].attrs['href']
-            wget.download(link, './Anexos')
+            link = site.select(f"#parent-fieldname-text > p:nth-child({str(x)}) > a")[0].attrs["href"]
+            wget.download(link, "./Anexos")
 
         for arquivo in Path("./Anexos").glob("*.*"):
             arquivos.append(arquivo)
--- segundo.py
+++ segundo.py
@@ -1,10 +1,41 @@
-import os
 import Utils.utils as utils
-import camelot #pip3 install camelot-py[cv] tabula-py
+import os
+import tabula
+import csv
 
-if os.path.isdir('./Anexos') == True:
+if os.path.isdir("./Anexos") == True:
     arquivos_anexo = utils.listar_arquivos("./Anexos", "*.*")
     utils.excluir_arquivos(arquivos_anexo)
     os.rmdir("./Anexos")
 
 utils.extrair_rar("Anexos.zip")
+
+arquivoPdf = "Anexos/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537_RN538_RN539_RN541_RN542_RN544_546.pdf"
+tabula.convert_into(arquivoPdf, "Anexo_I_Original.csv", output_format="csv", pages="3-180", lattice=True)
+
+conteudo = ""
+cabecalho_parte1 = ""
+cabecalho_parte2 = ""
+
+with open("Anexo_I_Original.csv", newline="") as aquivocsv:
+    anexo_Original = csv.reader(aquivocsv, delimiter=" ", quotechar="|")
+    for linha in anexo_Original:
+        if anexo_Original.line_num == 1:
+            cabecalho_parte1 = linha[0]
+            conteudo = conteudo + ", ".join(linha) + "\n"
+
+        elif anexo_Original.line_num == 2:
+            cabecalho_parte2 = linha[0]
+            conteudo = conteudo + ", ".join(linha) + "\n"
+
+        elif linha[0] != cabecalho_parte1 and linha[0] != cabecalho_parte2:
+            conteudo = conteudo + " ".join(linha) + "\n"
+
+    conteudo = conteudo.replace(",OD,", ",Seg. Odontológica,")
+    conteudo = conteudo.replace(",AMB,", ",Seg. Ambulatorial,")
+
+arquivo_csv = open("Anexo_I.csv", "w+")
+arquivo_csv.write(conteudo)
+arquivo_csv.close()
+
+os.remove(os.path.join("Anexo_I_Original.csv"))
